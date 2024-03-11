be88de2fb2014b1aa6c1a3dfc57d5456b2f23e00
artem <artembaranov.00@mail.ru>
2024-03-11 11:32:34 +0300

    added tab and some other fix

--- src/js/app/methods.js
+++ src/js/app/methods.js
@@ -1,39 +1,39 @@
 export function plus(a, b) {
-    const result = Number(a) + Number(b);
+  const result = Number(a) + Number(b);
 
-    if(Number.isInteger(result)) {
-        return result;
-    } else {
-        return parseFloat(result.toFixed(8));
-    }
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return parseFloat(result.toFixed(8));
+  }
 }
 
 export function minus(a, b) {
-    const result = Number(a) - Number(b);
+  const result = Number(a) - Number(b);
 
-    if(Number.isInteger(result)) {
-        return result;
-    } else {
-        return parseFloat(result.toFixed(8));
-    }
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return parseFloat(result.toFixed(8));
+  }
 }
 
 export function devide(a, b) {
-    const result = Number(a) / Number(b);
+  const result = Number(a) / Number(b);
 
-    if(Number.isInteger(result)) {
-        return result;
-    } else {
-        return parseFloat(result.toFixed(8));
-    }
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return parseFloat(result.toFixed(8));
+  }
 }
 
 export function multiply(a, b) {
-    const result = Number(a) * Number(b);
+  const result = Number(a) * Number(b);
 
-    if(Number.isInteger(result)) {
-        return result;
-    } else {
-        return parseFloat(result.toFixed(8));
-    }
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return parseFloat(result.toFixed(8));
+  }
 }
